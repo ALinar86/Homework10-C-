468d920f48626a8fb8f3945bad9d1623599a9d1d
Linar <alr86@mail.ru>
2022-07-29 10:14:44 +0300

    Added task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int FindVowel(string[] phrase)
+{
+    int count = 0;
+    string[] vowel = { "A", "a", "E", "e", "I", "i", "U", "u", "Y", "y", "O", "o" };
+    for (int i = 0; i < phrase.Length; i++)
+        for (int j = 0; j < vowel.Length; j++)
+            if (phrase[i][0] == vowel[j][0]) count++;
+    return count;
+}
+
+string[] words = { "Awe", "aer", "srt", "rty", "tyu", "Ontr", "yje", "ddf", "gfd" };
+Console.WriteLine($"The number of words in the array starting with a vowel letter:  {FindVowel(words)}");
